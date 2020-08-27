7aa1ad904386652a9d0ab6073c848a2f4de73a62
sanjatinjak <tinjaksanja@gmail.com>
2020-08-27 14:32:02 +0200

    dodan Http Basic Auth

--- rest/java/src/main/java/com/example/rnwauniversity/SecurityConfiguration.java
+++ rest/java/src/main/java/com/example/rnwauniversity/SecurityConfiguration.java
@@ -1,20 +1,41 @@
 package com.example.rnwauniversity;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
 
 @Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
-public class SecurityConfiguration  {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception 
+    {
+        http
+         .csrf().disable()
+         .authorizeRequests().anyRequest().authenticated()
+         .and()
+         .httpBasic();
+    }
+  
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) 
+            throws Exception 
+    {
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password("{noop}password")
+            .roles("USER");
+    }
 
-
-    /*@Autowired
-    public SecurityConfiguration(@Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }*/
-
-
-   
 
 
 }
